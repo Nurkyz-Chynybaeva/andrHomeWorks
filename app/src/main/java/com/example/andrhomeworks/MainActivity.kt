dd4a34f032edaad20bab6bfc15c97f87a4dcd8f3
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-12-04 23:23:50 +0600

    9th home work

--- app/src/main/java/com/example/andrhomeworks/MainActivity.kt
+++ app/src/main/java/com/example/andrhomeworks/MainActivity.kt
@@ -1,14 +1,39 @@
 package com.example.andrhomeworks
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatTextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var text: AppCompatTextView
+    private var count: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        text = findViewById(R.id.txt)
+        val btn1 = findViewById<AppCompatButton>(R.id.btn1)
+        val btn2 = findViewById<AppCompatButton>(R.id.btn2)
+        btn1.setOnClickListener{
+            ++count
+            text.text = count.toString()
+        }
+        btn2.setOnClickListener {
+            if (text.text != "0") {
+                text.text = "0"
+                count = 0
+            }
+        }
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("key", count)
+    }
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        count = savedInstanceState.getInt("key")
+        text.text = count.toString()
     }
 }
+
