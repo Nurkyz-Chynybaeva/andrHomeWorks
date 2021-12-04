0c453eff97571c7f017d5558ab4a875aca2d6354
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-12-04 23:21:40 +0600

    andrHomeWorks

---
+++ app/src/main/java/com/example/andrhomeworks/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.andrhomeworks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatTextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
