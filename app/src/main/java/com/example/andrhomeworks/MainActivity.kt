777eb1d03890abda13b7db116c53966c5985314f
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-12-10 17:56:26 +0600

    13th lesson

--- app/src/main/java/com/example/andrhomeworks/MainActivity.kt
+++ app/src/main/java/com/example/andrhomeworks/MainActivity.kt
@@ -2,27 +2,10 @@ package com.example.andrhomeworks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
-class MainActivity : AppCompatActivity(), OnClickButton {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager.beginTransaction()
-            .add(R.id.container, Fragment1())
-            .commit()
-    }
-
-
-    override fun onClick(name: String, password: String) {
-        val fragment2 = Fragment2()
-        val bundle = Bundle()
-        bundle.putString("text", name)
-        bundle.putString("number", password)
-        fragment2.arguments = bundle
-        supportFragmentManager.beginTransaction()
-            .add(R.id.container, fragment2)
-            .addToBackStack(null)
-            .commit()
     }
 }
