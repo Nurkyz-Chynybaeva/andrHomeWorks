6237157c074781dc7360cc5874578f46b1ffd54a
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-12-11 15:18:44 +0600

    18th lesson

--- app/src/main/java/com/example/andrhomeworks/Fragment1.kt
+++ app/src/main/java/com/example/andrhomeworks/Fragment1.kt
@@ -3,27 +3,23 @@ package com.example.andrhomeworks
 import android.content.Context
 import android.os.Bundle
 import android.view.View
-import androidx.appcompat.widget.AppCompatButton
-import androidx.appcompat.widget.AppCompatEditText
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 
 class Fragment1 : Fragment(R.layout.fragment1) {
-    private lateinit var listener: OnClickButton
     override fun onAttach(context: Context) {
         super.onAttach(context)
-        listener = context as OnClickButton
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val toolbar =
+            view.findViewById<Toolbar>(R.id.toolbar)      //указали toolbar вместо actionBar и все свойства
+        setSupportActionBar(toolbar)
 
-        val btn2 = view.findViewById<AppCompatButton>(R.id.btn2F1)
-        val name = view.findViewById<AppCompatEditText>(R.id.edit1)
-        val password = view.findViewById<AppCompatEditText>(R.id.edit2)
-        btn2.setOnClickListener {
-            if (name.text.toString().isNotEmpty() && password.text.toString().isNotEmpty()) {
+    }
 
-            }
-        }
+    private fun setSupportActionBar(toolbar: Any) {
+
     }
 }
--- app/src/main/java/com/example/andrhomeworks/MainActivity.kt
+++ app/src/main/java/com/example/andrhomeworks/MainActivity.kt
@@ -2,27 +2,48 @@ package com.example.andrhomeworks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 
-class MainActivity : AppCompatActivity(), OnClickButton {
+
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager.beginTransaction()
-            .add(R.id.container, Fragment1())
-            .commit()
+        val toolbar =
+            findViewById<Toolbar>(R.id.toolbar)      //указали toolbar вместо actionBar и все свойства
+        setSupportActionBar(toolbar)                           //actionBar активити передаст к Toolbar
     }
 
-
-    override fun onClick(name: String, password: String) {
-        val fragment2 = Fragment2()
-        val bundle = Bundle()
-        bundle.putString("text", name)
-        bundle.putString("number", password)
-        fragment2.arguments = bundle
-        supportFragmentManager.beginTransaction()
-            .add(R.id.container, fragment2)
-            .addToBackStack(null)
-            .commit()
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+//        menu?.add("menu 10")                                       //програмно создали
+//        menu?.add("menu 20")
+//        menu?.add("menu 30")
+        return super.onCreateOptionsMenu(menu)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {    //этот метод вызывается тогда, когда кликаем на пункты меню
+        when (item.itemId) {
+            R.id.menu1 -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragmentContainer, Fragment1())
+                    .commit()
+            }
+            R.id.menu2 -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragmentContainer, Fragment2())
+                    .commit()
+            }
+
+            else -> {
+            }
+        }
+        Toast.makeText(this, item.title, Toast.LENGTH_SHORT).show()
+        return super.onOptionsItemSelected(item)
+    }
+
 }
