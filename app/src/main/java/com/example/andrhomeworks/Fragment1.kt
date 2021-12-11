a27e11b07e907866c305fea277cf59955dc83994
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-12-11 18:02:57 +0600

    hw18

--- app/src/main/java/com/example/andrhomeworks/Fragment1.kt
+++ app/src/main/java/com/example/andrhomeworks/Fragment1.kt
@@ -2,6 +2,9 @@ package com.example.andrhomeworks
 
 import android.content.Context
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
@@ -14,12 +17,44 @@ class Fragment1 : Fragment(R.layout.fragment1) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val toolbar =
-            view.findViewById<Toolbar>(R.id.toolbar)      //указали toolbar вместо actionBar и все свойства
-        setSupportActionBar(toolbar)
+            view.findViewById<Toolbar>(R.id.toolbarF1)
+
+//        toolbar.setNavigationIcon(androidx.appcompat.R.drawable.abc_ic_ab_back_material)
+//        toolbar.setNavigationOnClickListener {
+//            // --
+//        }
+
+        toolbar.inflateMenu(R.menu.menu)
+        toolbar.setOnMenuItemClickListener {
+            //
+        }
 
     }
 
-    private fun setSupportActionBar(toolbar: Any) {
-
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
     }
+
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//
+//            R.id.menu1 -> {
+//                supportFragmentManager.beginTransaction()
+//                    .replace(R.id.fragmentContainer, Fragment1() )
+//                    .commit()
+//            }
+//
+//        return super.onOptionsItemSelected(item)
+//    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- app/src/main/java/com/example/andrhomeworks/MainActivity.kt
+++ app/src/main/java/com/example/andrhomeworks/MainActivity.kt
@@ -2,48 +2,14 @@ package com.example.andrhomeworks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.Toast
-import androidx.appcompat.widget.Toolbar
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val toolbar =
-            findViewById<Toolbar>(R.id.toolbar)      //указали toolbar вместо actionBar и все свойства
-        setSupportActionBar(toolbar)                           //actionBar активити передаст к Toolbar
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragmentContainer, Fragment1())
+            .commit()
     }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu, menu)
-//        menu?.add("menu 10")                                       //програмно создали
-//        menu?.add("menu 20")
-//        menu?.add("menu 30")
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {    //этот метод вызывается тогда, когда кликаем на пункты меню
-        when (item.itemId) {
-            R.id.menu1 -> {
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.fragmentContainer, Fragment1())
-                    .commit()
-            }
-            R.id.menu2 -> {
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.fragmentContainer, Fragment2())
-                    .commit()
-            }
-
-            else -> {
-            }
-        }
-        Toast.makeText(this, item.title, Toast.LENGTH_SHORT).show()
-        return super.onOptionsItemSelected(item)
-    }
-
 }
