678898956260daac491bb9aee8f1bc5c9ab59499
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-12-23 20:10:07 +0600

    added some changes

--- app/src/main/java/com/example/andrhomeworks/Fragment2.kt
+++ app/src/main/java/com/example/andrhomeworks/Fragment2.kt
@@ -1,20 +1,12 @@
 package com.example.andrhomeworks
 
-import android.content.Context
 import android.os.Bundle
 import android.view.View
-import androidx.appcompat.widget.AppCompatTextView
 import androidx.fragment.app.Fragment
 
-class Fragment2: Fragment(R.layout.fragment2) {
-    private lateinit var txt: AppCompatTextView
+class Fragment2 : Fragment(R.layout.fragment2) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        txt = view.findViewById(R.id.txtF2)
-
-        val text2 = arguments?.getString("key") ?: "default"
-        txt.text = text2
     }
-
 }
--- app/src/main/java/com/example/andrhomeworks/MainActivity.kt
+++ app/src/main/java/com/example/andrhomeworks/MainActivity.kt
@@ -2,26 +2,10 @@ package com.example.andrhomeworks
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity(), OnItemClick {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager.beginTransaction()
-            .add(R.id.containerFrag, Fragment1())
-            .commit()
-    }
-
-    override fun onClick(enteredText: String) {
-        val fragment2 = Fragment2()
-
-        val bundle = Bundle()
-        bundle.putString("key", enteredText)
-        fragment2.arguments = bundle
-
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.containerFrag, fragment2)
-            .addToBackStack(null)
-            .commit()
     }
 }
