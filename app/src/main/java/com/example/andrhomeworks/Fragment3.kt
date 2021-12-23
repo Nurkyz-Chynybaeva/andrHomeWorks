b27736a705f5ca914658c5afe12c1c251f3a3cbf
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-12-24 02:27:06 +0600

    hw 31

--- app/src/main/java/com/example/andrhomeworks/Fragment3.kt
+++ app/src/main/java/com/example/andrhomeworks/Fragment3.kt
@@ -5,6 +5,7 @@ import android.view.View
 import androidx.fragment.app.Fragment
 
 class Fragment3: Fragment(R.layout.fragment3) {
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
