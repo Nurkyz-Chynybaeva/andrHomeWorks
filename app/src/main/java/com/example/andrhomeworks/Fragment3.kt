a1d108303c8c8309ae174aa9981954dfde5fcb19
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-12-24 00:43:19 +0600

    31 hw

---
+++ app/src/main/java/com/example/andrhomeworks/Fragment3.kt
@@ -0,0 +1,11 @@
+package com.example.andrhomeworks
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class Fragment3: Fragment(R.layout.fragment3) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
