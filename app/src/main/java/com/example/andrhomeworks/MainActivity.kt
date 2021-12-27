c1cf5ab79cce4671f8e3ebb653a1c675515a5b83
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-12-27 21:32:55 +0600

    34th hw

--- app/src/main/java/com/example/andrhomeworks/MainActivity.kt
+++ app/src/main/java/com/example/andrhomeworks/MainActivity.kt
@@ -5,31 +5,22 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatEditText
 import androidx.appcompat.widget.AppCompatTextView
-import java.util.regex.Matcher
-import java.util.regex.Pattern
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var txt: AppCompatTextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        txt = findViewById(R.id.txt)
+        val txt = findViewById<AppCompatTextView>(R.id.txt)
         val edit = findViewById<AppCompatEditText>(R.id.edit)
         val btn = findViewById<AppCompatButton>(R.id.btn)
 
         btn.setOnClickListener {
-            val pattern: Pattern = Pattern.compile("О.+?о")
-
-            val eTxt = edit.text.toString()
-            val i = if (eTxt.isEmpty()) 0 else eTxt.split("o\\+".toRegex()).size
-            txt.text = i.toString()
-
-            pattern.matcher(eTxt)
-
+            val eTxt = edit.text.toString().trim()
+            val count = eTxt.split("[a-zA-Z]*[oO][a-zA-Z]*".toRegex()).size - 1
+            txt.text = count.toString()
         }
     }
 }
-
 
