ca6afab5f2f50c56d06f4720b56137be883eeeb5
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-12-29 10:37:03 +0600

    36th home work

--- app/src/main/java/com/example/andrhomeworks/MainActivity.kt
+++ app/src/main/java/com/example/andrhomeworks/MainActivity.kt
@@ -1,35 +1,25 @@
 package com.example.andrhomeworks
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import androidx.appcompat.widget.AppCompatTextView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var launcher: ActivityResultLauncher<Intent>
-    private lateinit var txt: AppCompatTextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        registerLauncher()
-
-        txt = findViewById(R.id.txt)
+        val txt = findViewById<AppCompatTextView>(R.id.txt)
+        val edit = findViewById<AppCompatEditText>(R.id.edit)
         val btn = findViewById<AppCompatButton>(R.id.btn)
-        btn.setOnClickListener {
-            launcher.launch(Intent(this, MainActivity2::class.java))
-        }
-    }
 
-    private fun registerLauncher() {
-        launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-            if (it.resultCode == RESULT_OK) {
-                txt.text = it.data?.getStringExtra("KEY") ?: "default"
-            }
+        btn.setOnClickListener {
+            val eTxt = edit.text.toString()
+            val count = eTxt.split("[ая]\\b".toRegex()).size - 1
+            txt.text = count.toString()
         }
     }
 }
