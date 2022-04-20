9a81a2efdcb6a4b2f9919e4f688e833cfe925bf8
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2022-04-20 14:21:56 +0600

    73

--- app/src/main/java/com/example/andrhomeworks/di/modules/LocalModule.kt
+++ app/src/main/java/com/example/andrhomeworks/di/modules/LocalModule.kt
@@ -1,11 +1,15 @@
 package com.example.andrhomeworks.di.modules
 
+import android.content.Context
+import androidx.room.Room
 import com.example.andrhomeworks.data.local.AppDatabase
 import com.example.andrhomeworks.data.local.CharacterDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -16,4 +20,14 @@ class StorageModule {
         return appDatabase.characterDao()
     }
 
+    @Singleton
+    @Provides
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ): AppDatabase{
+        return Room.databaseBuilder(context, AppDatabase::class.java, "db")
+            .fallbackToDestructiveMigration()
+            .build()
+    }
 }
+// annotation Singleton, anshn context,
--- app/src/main/java/com/example/andrhomeworks/di/modules/NetworkModule.kt
+++ app/src/main/java/com/example/andrhomeworks/di/modules/NetworkModule.kt
@@ -1,10 +1,13 @@
 package com.example.andrhomeworks.di.modules
 
+import com.example.andrhomeworks.data.network.Api
 import com.example.andrhomeworks.di.annotations.ApiWithInterceptor
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -12,5 +15,11 @@ class NetworkModule {
 
     @Provides
     @ApiWithInterceptor
-    fun provideApiWithInterceptor
+    fun provideApiWithInterceptor(
+        retrofit: Retrofit
+    ): Api{
+        return retrofit.create(Api::class.java)
+    }
+
+
 }
